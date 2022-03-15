7ce8b203ea95c7085993b946a5ad5e3d39a3454c
banku27 <pankajparihar123321@gmail.com>
2022-03-16 00:20:42 +0530

    payment page completed

--- lib/pages/payment_page.dart
+++ lib/pages/payment_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:payment_integration/components/colors.dart';
+import 'package:payment_integration/widget/buttons.dart';
+import 'package:payment_integration/widget/large_button.dart';
 
 class PaymentPage extends StatelessWidget {
   const PaymentPage({Key? key}) : super(key: key);
@@ -38,7 +40,7 @@ class PaymentPage extends StatelessWidget {
                   color: AppColor.mainColor,
                   fontWeight: FontWeight.bold),
             ),
-            Text(
+            const Text(
               'Payment is completed for 2 bills',
               style: TextStyle(
                   fontSize: 20,
@@ -56,79 +58,141 @@ class PaymentPage extends StatelessWidget {
                 border:
                     Border.all(width: 2, color: Colors.grey.withOpacity(0.5)),
               ),
-              child: Column(
-                children: [
-                  Row(
+              child: MediaQuery.removePadding(
+                context: context,
+                removeTop: true,
+                removeBottom: true,
+                child: ListView.builder(
+                  itemCount: 2,
+                  itemBuilder: (context, index) => Column(
                     children: [
-                      Container(
-                        margin: EdgeInsets.only(top: 15, left: 20, bottom: 10),
-                        height: 50,
-                        width: 50,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(25),
-                          color: Colors.green,
-                        ),
-                        child: const Icon(
-                          Icons.done,
-                          size: 30,
-                        ),
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                      Row(
                         children: [
-                          const Text(
-                            'KenGen Power',
-                            style: TextStyle(
-                                color: AppColor.mainColor,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w700),
+                          Container(
+                            margin:
+                                EdgeInsets.only(top: 15, left: 20, bottom: 10),
+                            height: 50,
+                            width: 50,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(25),
+                              color: Colors.green,
+                            ),
+                            child: const Icon(
+                              Icons.done,
+                              size: 30,
+                            ),
                           ),
                           SizedBox(
-                            height: 10,
+                            width: 10,
                           ),
-                          Text('ID:7389601585',
-                              style: TextStyle(
-                                  color: AppColor.idColor,
-                                  fontSize: 13,
-                                  fontWeight: FontWeight.w700))
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                'KenGen Power',
+                                style: TextStyle(
+                                    color: AppColor.mainColor,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w700),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text('ID:7389601585',
+                                  style: TextStyle(
+                                      color: AppColor.idColor,
+                                      fontSize: 13,
+                                      fontWeight: FontWeight.w700))
+                            ],
+                          ),
+                          const SizedBox(
+                            width: 40,
+                          ),
+                          Column(
+                            children: const [
+                              Text(
+                                '',
+                                style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w700),
+                              ),
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                '\$1907.00',
+                                style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w700),
+                              ),
+                            ],
+                          ),
                         ],
                       ),
-                      SizedBox(
-                        width: 40,
-                      ),
-                      Column(
-                        children: const [
-                          Text(
-                            '',
-                            style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w700),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            '\$1907.00',
-                            style: TextStyle(
-                                color: Colors.grey,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w700),
-                          ),
-                        ],
-                      ),
+                      Divider(
+                        thickness: 2,
+                        color: Colors.grey.withOpacity(0.5),
+                      )
                     ],
                   ),
-                  Divider(
-                    thickness: 2,
-                    color: Colors.grey.withOpacity(0.5),
-                  )
-                ],
+                ),
               ),
             ),
+            SizedBox(
+              height: h * 0.05,
+            ),
+            Column(children: [
+              Text(
+                "Total Amount",
+                style: TextStyle(
+                    fontSize: 16,
+                    color: AppColor.idColor,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                "\$7319.00",
+                style: TextStyle(
+                    fontSize: 26,
+                    color: AppColor.mainColor,
+                    fontWeight: FontWeight.w700),
+              ),
+              SizedBox(
+                height: h * 0.14,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  AppButtons(
+                    icon: Icons.share_sharp,
+                    onTap: () {},
+                    text: 'Share',
+                    iconColor: AppColor.selectColor,
+                  ),
+                  SizedBox(
+                    width: h * 0.04,
+                  ),
+                  AppButtons(
+                    icon: Icons.print_outlined,
+                    onTap: () {},
+                    text: 'Print',
+                    iconColor: AppColor.selectColor,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              AppLargeButton(
+                text: 'Done',
+                backgroundColor: Colors.white,
+                textColor: Colors.white,
+              )
+            ])
           ],
         ),
       ),
